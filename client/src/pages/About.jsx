931ac3c3a9cbf76584deba2f12af8cbbe8992c4d
Malekbennasser <malekben0001@gmail.com>
2024-02-16 16:47:01 +0100

    compete about and projects pages

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,3 +1,35 @@
 export default function About() {
-  return <div>About</div>;
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="max-w-2xl mx-auto p-3 text-center">
+        <div>
+          <h1 className="text-3xl font font-semibold text-center my-7">
+            About Mern Blog
+          </h1>
+          <div className="text-md text-gray-500 flex flex-col gap-6">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Perferendis adipisci vitae ut eaque, corporis cupiditate totam,
+              esse odio dicta minus qui! Maiores, repudiandae veniam est omnis
+              eveniet fuga consectetur? Reiciendis!
+            </p>
+
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos,
+              ipsam. Architecto, sequi atque laboriosam suscipit minus aliquid
+              debitis porro laudantium esse quos eum odio voluptates cum vero
+              animi officiis sint!
+            </p>
+
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aperiam
+              voluptatibus tenetur voluptas culpa, ducimus enim nostrum.
+              Perspiciatis dicta asperiores laboriosam aperiam, odit incidunt
+              magnam ipsam commodi ipsa nisi molestiae fuga?
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
